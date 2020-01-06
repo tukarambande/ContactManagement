adc77654221e175b94b368736afc598dd48e4e8d
tukarambande <tukarambande>
2020-01-07 00:02:32 +0530

    added sql query file

---
+++ ContactManagement.Service.DLL/SqlQuery/ContactMgmtQueries.sql
@@ -0,0 +1,18 @@
+﻿
+--CREATE DATABASE ContactManagement;
+
+USE ContactsManagement
+GO
+
+--CREATE TABLE [contactInfo](
+--	[contact_id] [int] IDENTITY(1,1) primary key,
+--	[first_name] [nvarchar](50) NOT NULL,
+--	[last_name] [nvarchar](10) NOT NULL,
+--	[email] [nvarchar](256) NULL,
+--	[phone_number] [nvarchar](15) NULL,
+--	[status] [BIT] not null,
+--	[address] [nvarchar](1128) null,
+--) ON [PRIMARY]
+--GO
+
+select * from ContactInfo;
